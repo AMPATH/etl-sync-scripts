8e0ad1624efe2c57983ff7e58edd7be7e2dbb2a5
Fatma Ali <tumz92@gmail.com>
2019-05-24 03:47:01 +0300

    Updated Script

--- appointments-job.js
+++ appointments-job.js
@@ -1,9 +1,13 @@
 let BuildAppointments = require('./build-appointments');
+let ScheduleAppointments = require('./schedule-appointments');
 let Moment = require('moment');
 try {
     let buildJob = new BuildAppointments();
+    let scheduleJob = new ScheduleAppointments();
+
     let startedAt = Moment();
     console.log('Starting at:', startedAt.toLocaleString())
+    scheduleJob.runJob().then(() => {
     buildJob.runJob().then(() => {
         console.log('Finshed all jobs.');
         let endedAt = Moment();
@@ -14,7 +18,11 @@ try {
         console.error('error running appointments job');
         throw err; 
         process.exit(1)
-    });
+    })}).catch((err) => {
+        console.error('error scheduling appointments');
+        throw err;
+        process.exit(1);
+    })
 } catch (error) {
     console.error('Error running pipeline', error);
 }
--- hiv-summary-job.js
+++ hiv-summary-job.js
@@ -3,8 +3,11 @@ let ScheduleHivSummary = require('./schedule-hiv-summary');
 let Moment = require('moment');
 try {
     let buildJob = new BuildHivSummary();
+    let scheduleJob = new ScheduleHivSummary();
     let startedAt = Moment();
     console.log('Starting at:', startedAt.toLocaleString())
+
+    scheduleJob.runJob().then(() => {
     buildJob.runJob().then(() => {
         console.log('Finshed all jobs.');
         let endedAt = Moment();
@@ -15,7 +18,12 @@ try {
         console.error('error running hiv summary job');
         throw err; 
         process.exit(1)
+    })}).catch((err) => {
+        console.error('error scheduling hiv summary');
+        throw err;
+        process.exit(1);
     });
 } catch (error) {
     console.error('Error running pipeline', error);
+    process.exit(1);
 }
